f520d27740683c7464b975bf7be9b993ad56c8ec
Artem Drashpul <adrashpul@dataart.com>
2017-12-21 12:19:32 +0200

    Added basic socket events

--- server/server.js
+++ server/server.js
@@ -1,18 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath)); //configures to use public as an index
-app.use(bodyParser.json());
 
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
-app.listen(port, ()=>{
+    socket.on('disconnect', ()=>{
+        console.log('Disconnected from server');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
-})
+});
 
 
 
